bd23b1fb163df9fff323d3deacf9def36503de17
Muhammad Sumon Molla Selim <smseleem@gmail.com>
2019-03-18 20:18:46 +0600

    ProductCategory model added, migration updated

--- database/migrations/2019_03_13_203800_create_product_categories_table.php
+++ database/migrations/2019_03_13_203800_create_product_categories_table.php
@@ -11,7 +11,7 @@ class CreateProductCategoriesTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::create('product_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
@@ -27,7 +27,7 @@ class CreateProductCategoriesTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('product_categories');
     }
